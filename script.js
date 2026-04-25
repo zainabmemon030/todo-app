c3b5c2adc320419a2e2c78f86c34b11117432b34
zainabmemon030 <zainab.memon030@gmail.com>
2026-04-25 20:51:43 +0500

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
+let tasks = [];
+let currentFilter = "all";
+
 function addTask() {
     let input = document.getElementById("taskInput");
-    let task = input.value;
+    let task = input.value.trim();
 
     if (task === "") return;
 
-    let li = document.createElement("li");
-    li.textContent = task;
-
-    li.onclick = function () {
-        li.remove();
-    };
-
-    document.getElementById("taskList").appendChild(li);
+    tasks.push({
+        text: task,
+        done: false
+    });
 
     input.value = "";
+    renderTasks();
+}
+
+function renderTasks() {
+    let list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    let filteredTasks = tasks;
+
+    if (currentFilter === "active") {
+        filteredTasks = tasks.filter(t => !t.done);
+    } else if (currentFilter === "done") {
+        filteredTasks = tasks.filter(t => t.done);
+    }
+
+    filteredTasks.forEach((task, index) => {
+        let li = document.createElement("li");
+
+        if (task.done) {
+            li.classList.add("done");
+        }
+
+        li.textContent = task.text;
+
+        // click to toggle done
+        li.onclick = function () {
+            task.done = !task.done;
+            renderTasks();
+        };
+
+        list.appendChild(li);
+    });
+}
+
+function filterTasks(type) {
+    currentFilter = type;
+    renderTasks();
 }
